64840593895cb38b7bd740b8c0764c7e54681560
Raymond Tay <raymondtay1974@gmail.com>
2018-06-19 15:41:24 +0800

    Added travis ci

--- src/main/scala/hicoden/jobgraph/fsm/runners/Runner.scala
+++ src/main/scala/hicoden/jobgraph/fsm/runners/Runner.scala
@@ -59,6 +59,53 @@ class DataflowRunner extends ExecRunner {
 
 }
 
+// JobGraph Dataflow runner and this type of runner basically hooks to a
+// running Apache Mesos cluster and feeds it stuff on what it needs to know.
+//
+class MesosDataflowRunner extends ExecRunner {
+
+  import cats._, data._, implicits._
+
+  val logger = Logger(classOf[MesosDataflowRunner])
+
+  /**
+   * The runtime representation of the context would be created from the job's
+   * configuration via [[f]] and once its done, jobgraph would launch the
+   * process and captures whatever the output it produces. For now, jobgraph is
+   * not expecting any kind of return back to the service - this might change
+   * in the future.
+   * @param ctx the job's configuration
+   * @param f the transformer function that converts the configuration to
+   *          a configuration that is only available during the execution of
+   *          the job
+   * @return A future which carries the [[JobContext]] as payload
+   */
+  def run(ctx: ExecContext)(f: JobConfig ⇒ JobContext)(implicit ec: ExecutionContext) : Future[JobContext] = Future {
+    import Functions._
+    val runtimeContext = f(ctx.jobConfig)
+
+    val result : Either[Throwable,scala.sys.process.Process] = scala.util.Try{
+      val (command, cwd, env) = buildCommand(runtimeContext)
+      if (cwd.isEmpty) Process(command, None, env.toSeq:_*).run()
+      else Process(command, Some(new java.io.File(cwd)), env.toSeq:_*).run()
+    }.toEither
+    result.fold(onError(runtimeContext), onSuccess(runtimeContext))  
+  }
+ 
+  // If error occurs, a log is produced and we do not alter the context but
+  // return it
+  def onError(ctx: runtime.JobContext) = (error: Throwable) ⇒ {
+    logger.error(s"Unable to trigger program with this error: ${error.getStackTrace}")
+    ctx
+  }
+
+  def onSuccess(ctx: runtime.JobContext) = (proc: scala.sys.process.Process) ⇒ {
+    logger.info("About to parse the return data and validate it.")
+    ctx
+  }
+
+}
+
 // 
 // JobGraph DataflowMonitor Runner. See [[DataflowMonitorRunnerSpecs]] for the unit tests
 // or check out the sample code via `sbt tut` on the sbt repl.
